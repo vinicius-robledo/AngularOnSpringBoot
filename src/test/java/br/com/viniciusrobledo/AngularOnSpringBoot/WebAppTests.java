ac5d85b3dbf37ea28c255323ee1fb9e69e4c21bc
Vinicius Robledo <vinicius.robledo@fatec.sp.gov.br>
2020-08-13 16:24:25 -0300

    Classes Java Spring e POM

---
+++ src/test/java/br/com/viniciusrobledo/AngularOnSpringBoot/WebAppTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusrobledo.AngularOnSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
